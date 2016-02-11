c0bb3d02e1a14caf588864bc2becd3898719b33f
MOSAVI17 <www.mosavi7857@gmail.com>
2016-02-11 13:07:15 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 $config = array(
-  'token' => 'YOUR TOKEN',
-  'bot_username' => 'YOUR BOT USERNAME',
+  'token' => '198679057:AAFREUAV89HA2pcNBYRhjbiGdev0k8cKyBM',
+  'bot_username' => '198679057',
   'db' => 'rfiles.sqlite'
   );
